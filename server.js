3a1b9321fcdb1ea6e87a9274338ea5b09e5196b5
Luiz Silva. <160427605+luizOASSilva@users.noreply.github.com>
2025-03-16 12:36:20 -0300

    Criando, routerizando e conectando ao banco de dados

---
+++ server.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+
+const path = require('path');
+const express = require('express');
+const app = express();
+const router = require('./router');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(resp => app.emit('successfully'))
+    .catch(e => console.log(e));
+
+const sessionConfig = session({
+    secret: process.env.SECRET_SESSION,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongoUrl: process.env.MONGO_URI
+        
+    })
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true, 
+    }
+});
+
+app.use(sessionConfig);
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(router);
+
+app.on('successfully', () => app.listen(3000, () => console.log('https://localhost:3000')));
